41270128e17bbff44b2a1e5aa0a3cb99dc0bfbe4
Abduqodir <abduqodirxojiakbarov202gmail.com>
2024-06-20 13:44:51 +0500

    Abduqodir added

---
+++ src/main/java/org/example/clickup/model/View.java
@@ -0,0 +1,16 @@
+package org.example.clickup.model;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+
+public class View extends ClickApps {
+}
